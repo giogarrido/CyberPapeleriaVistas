ad44c89f84f1e2c6774fdd6f401c06eaf967b654
Garrido <giovanni.garrido228724@potros.itson.edu.mx>
2023-02-22 00:58:33 -0700

    form Venta

---
+++ src/vistaspapeleria/VistasPapeleria.java
@@ -0,0 +1,18 @@
+
+package vistaspapeleria;
+
+/**
+ *
+ * @author Giovanni Garrido
+ * ITSON
+ */
+public class VistasPapeleria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
